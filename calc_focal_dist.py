a3f7eb9fcaf50e2bf82cf9a3f68b921d5397a77a
Max Koch <max.koch@phys.uni-goettingen.de>
2018-11-09 17:44:15 +0100

    add mirror capability

--- calc_focal_dist.py
+++ calc_focal_dist.py
@@ -168,6 +168,73 @@ class Lens(object):
         plt.plot(lx,ly)
         plt.plot(rx,ly)
         
+class Mirror(Lens):
+    def __init__(self, offset_x=0., h=1e-2):
+        self.offset_x=offset_x
+        self.h = h
+        
+    def left_x(self):
+        return
+        
+    def _f(self, ray_inst, sidefunc, y):
+        return Lens()._f(ray_inst, sidefunc, y)
+    
+    def Newtonstep(self,ray_inst,sidefunc,yn):
+        return Lens().Newtonstep(ray_inst,sidefunc,yn)
+    
+    def Newton_find_yzero(self, ray_inst, sidefunc, yn = 0.):
+        return Lens().Newton_find_yzero(ray_inst, sidefunc, yn = 0.)
+            
+    def _rot_counterclock(self, phi, vec):
+        return Lens()._rot_counterclock(phi, vec)
+            
+
+    def refract_ray_curved(self,ray_inst, water_x):
+        for sidefunc in [self.left_x]:
+            if DEBUG: print "-- Mirror"
+            willhit = True
+            yhit = self.Newton_find_yzero(ray_inst, sidefunc)
+            xhit = sidefunc(yhit)
+            if np.abs(yhit) > self.h/2.: willhit= False
+            if willhit:
+                if DEBUG : print "- hit and reflecting"
+                # tangentiale und orthonormale berechnen und plotten
+                lens_deriv_x = sidefunc(yhit+1e-6) - sidefunc(yhit-1e-6)
+                lens_tangential = np.array([lens_deriv_x,2e-6])
+                lens_tangential /= np.linalg.norm(lens_tangential)
+                lens_orthonormal = np.array([lens_tangential[1],-lens_tangential[0]])
+                if lens_orthonormal[0] > 1e-8: 
+                    lens_orthonormal *=-1
+                if DEBUG: plt.arrow(xhit,yhit,lens_orthonormal[0]*1e-3,lens_orthonormal[1]*1e-3,head_width=0.,width=1e-4)
+                nray = np.array([ray_inst.nx,ray_inst.ny])
+                angle_left = np.arccos(nray.dot(lens_orthonormal))
+                if angle_left > np.pi/2.: 
+                    angle_left = np.pi - angle_left
+                nray = np.array([-ray_inst.nx,-ray_inst.ny])
+                angle_tilt = 2.*(angle_left)
+                nrot = np.array([0.,0.])
+                if (nray+lens_orthonormal)[1] < 0.:
+                    nrot = self._rot_counterclock(-angle_tilt,nray)
+                if (nray+lens_orthonormal)[1] >= 0.:
+                    nrot = self._rot_counterclock( angle_tilt,nray)
+                ray_inst.nx = nrot[0]
+                ray_inst.ny = nrot[1]
+                ray_inst.add_pos(xhit,yhit)
+                if DEBUG: print "- xhit: {}, yhit {}".format(xhit,yhit)
+                if DEBUG: print "- new nx: {}, ny {}".format(ray_inst.nx,ray_inst.ny)
+                #ray_inst.add_pos(xhit+nrot[0]*1e-3,yhit+nrot[1]*1e-3)
+                if DEBUGADV: plt.arrow(xhit,yhit,ray_inst.nx*1e-4,ray_inst.ny*1e-4,head_width=0.,width=0.3e-4,color="red")
+                
+            
+    def plot_mirror(self):
+        div = 30
+        lyl = range(div+1)
+        ly = [-self.h/2. + k*self.h/div for k in lyl]
+        lx = [self.left_x(y) for y in ly]
+        plt.plot(lx,ly)
+        
+    
+        
 class AsphLens(Lens):
     def __init__(self):
         self.info="thorlabs ACL7560U"
@@ -179,12 +246,12 @@ class AsphLens(Lens):
         self.offset_x = Lens().offset_x
         self.h = Lens().h
         
-    def left_x(self,y):
-        x = (y*y/(self.R*(1.+np.sqrt(1.-(1.+self.k)*y*y/self.R/self.R))) + self.A*y*y*y*y) + self.offset_x   #/17.8e-3*27.7e-3
+    def right_x(self,y):
+        x = -(y*y/(self.R*(1.+np.sqrt(1.-(1.+self.k)*y*y/self.R/self.R))) + self.A*y*y*y*y) + self.offset_x + self.d   #/17.8e-3*27.7e-3
         return x
     
-    def right_x(self,y):
-        return self.left_x(0.0) + self.d
+    def left_x(self,y):
+        return self.offset_x
     
 class SphLLens(Lens):
     def __init__(self, Rl, Rr, d):
@@ -247,7 +314,7 @@ class Beam(object):
             if i.use:
                 endx = self.endx
                 last_y = 0.
-                if i.ny > 2.*i.nx: endx = i.pos_x(-1) + 5e-3 
+                if np.abs(i.ny) > np.abs(2.*i.nx): endx = i.pos_x(-1) + 5e-3 
                 last_y = i.ny/i.nx * endx + (i.pos_y(-1) - i.ny/i.nx*i.pos_x(-1))
                 i.add_pos(endx,last_y)
                 if DEBUG: print "---END: num {}, last_x {}, last_y {}".format(num,endx,last_y)
@@ -271,15 +338,15 @@ class Beam(object):
                 lens.refract_ray_curved(ray,self.water_x)
                 
     def plot_rays(self):
-        for ray in self.rays:
+        for num,ray in enumerate(self.rays):
             if DEBUG: plt.plot(ray.x,ray.y,'o-')
-            else: plt.plot(ray.x,ray.y)
+            else: plt.plot(ray.x,ray.y,color=(np.mod(num-1,2),np.mod(num,2),0,1))
         
     
 class WaterLens(Beam,Lens):
-    def __init__(self):
+    def __init__(self,d=100e-3):
         self.h = Lens().h
-        self.d = 1e-3
+        self.d = d
         self.n = 1.333
         self.offset_x = Beam().water_x
             
@@ -288,6 +355,19 @@ class WaterLens(Beam,Lens):
     
     def right_x(self,y):
         return self.offset_x + self.d
+    
+class NegativeCuvette(Beam,Lens):
+    def __init__(self,d=120e-3,offset_x=0.0):
+        self.h = Lens().h
+        self.d = d
+        self.n = 1.333
+        self.offset_x = offset_x
+            
+    def left_x(self,y):
+        return self.offset_x
+    
+    def right_x(self,y):
+        return self.offset_x - self.d
     
     
 class Bubble(Lens):
@@ -306,19 +386,50 @@ class Bubble(Lens):
         if np.abs(y) > self.R: return 0
         return self.offset_x + np.sqrt(self.R**2 - y*y)
             
-        
+class MirrorF50(Mirror):
+    def __init__(self, offset_x=0., h=0.0254):
+        self.offset_x=offset_x
+        self.h = h
+        self.R = 0.1
+    
+    def left_x(self,y):
+        posx = np.sqrt(self.R*self.R - y*y) + self.offset_x - np.sqrt(self.R*self.R - self.h*self.h/4.)
+        return posx
+    
+class ParabMirror(Mirror):
+    def __init__(self, offset_x=0., h=0.0254, d=3e-3):
+        self.offset_x=offset_x
+        self.h = h
+        self.R = 0.1
+        self.d = d
+        self.coeff = 2.*self.d/self.h**2
+    
+    def left_x(self,y):
+        posx = -self.coeff*y*y + self.offset_x
+        return posx
         
 def main():
-    beam = Beam(width=1.99e-3,raycount = 20, water_x=-6e-3, startx=-6e-3, endx=10e-3)
+    #beam = Beam(width=1.99e-3,raycount = 20, water_x=-6e-3, startx=-6e-3, endx=10e-3)
     #beam = Beam(width=15e-3,raycount = 9, water_x=100e-3, startx=-100e-3, endx=100e-3)
-    h=0.0254
+    beam = Beam(width=15e-3,raycount = 9, water_x=1000e-3, startx=-100e-3, endx=-50e-3)
+    h=0.03
 
     #best_form_lens_f50_1inch = SphLLens(d=3.3e-3, Rl=0.172, Rr=30.1e-3)
     #best_form_lens_f50_1inch.h = h
     #best_form_lens_f50_1inch.offset_x = 0.
     #best_form_lens_f50_1inch.plot_lens()
     #beam.add_lens(best_form_lens_f50_1inch)
-
+    
+    #mirror = MirrorF50(offset_x=10e-3)
+    mirror = ParabMirror(offset_x=10e-3,d=2e-3,h=h)
+    mirror.plot_mirror()
+    beam.add_lens(mirror)
+    
+    water = NegativeCuvette(d=40e-3,offset_x=-10e-3)
+    water.h = h
+    water.plot_lens()
+    beam.add_lens(water)
+    
     #par = ParabLens(d=1e-3,a=20, b=0)
     #par.h = h
     #par.plot_lens()
@@ -330,9 +441,9 @@ def main():
     #asph.plot_lens()
     #beam.add_lens(asph)
 
-    bubble = Bubble(R=1e-3)
-    bubble.plot_lens()
-    beam.add_lens(bubble)
+    #bubble = Bubble(R=1e-3)
+    #bubble.plot_lens()
+    #beam.add_lens(bubble)
     
     #wat = WaterLens()
     #wat.h=h
